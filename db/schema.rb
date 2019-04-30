699494eab562af598a3a2a24f1aeb199ec2703f1
Firstname Lastname <e-mail address>
2019-04-29 19:51:34 -0700

    q1-5 db update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_30_014846) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rsvps", force: :cascade do |t|
     t.string "name"
     t.integer "head_count"
